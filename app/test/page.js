5ef80ef09f4ebd6c3786fa2192ee98192e91681f
ljj33 <ljj3347@naver.com>
2024-01-24 09:45:41 +0900

    240124

--- app/test/page.js
+++ app/test/page.js
@@ -1,21 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
+async function getData() {
+  const params = {
+    uid: "510418490939874600",
+  };
+  const url='http://localhost:3001/api/data'
+  const res = await axios.get(url, { params });
 
-async function getData(){
-  const res=await fetch('http://localhost:3001/api/data')
-
-  return res.json()
+  return res.data.result.data[0];
 }
 
 export default async function TestPage() {
+  const data = await getData();
+  console.log(data);
 
-  const tickets=await getData()
-  console.log(tickets)
   return (
     <div>
-      <h1 className='text-white'>Next.js API Data</h1>
-      <p className='text-white'>{tickets.message}</p>
+      <h1 className="text-white">Next.js API Data</h1>
+      <p className="text-white text-center">{data.totalCommission}</p>
     </div>
   );
 }
-
-
--- pages/api/data.js
+++ pages/api/data.js
@@ -1,5 +1,23 @@
-export default function handler(req, res) {
+import axios from 'axios';
+
+export default async function handler(req, res) {
+  const { uid } = req.query; // 쿼리 파라미터에서 uid 추출
   
-  
-  res.status(200).json({ message: 'Hello from the API' });
+  // uid 값이 없으면 에러 응답
+  if (!uid) {
+    return res.status(400).json({ message: 'UID is required' });
+  }
+
+  try {
+    const response = await axios.get('https://wdjc5gmr5voj7d3dqqwra3sa5m0zapso.lambda-url.ap-southeast-2.on.aws/', {
+      params: { uid },
+      headers: {
+        'accept': 'application/json'
+      }
+    });
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching external data', error: error.message });
+  }
 }
