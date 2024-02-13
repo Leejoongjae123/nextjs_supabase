8f735528e21473b2759cb559662223ab51ad5e29
ljj33 <ljj3347@naver.com>
2024-02-13 17:29:35 +0900

    240213_2

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -8,11 +8,13 @@ export async function GET(request: Request) {
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
+  
 
   if (code) {
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
     await supabase.auth.exchangeCodeForSession(code);
+    return NextResponse.redirect(requestUrl.origin)
   }
 
   // URL to redirect to after sign in process completes
