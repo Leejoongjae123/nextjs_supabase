62a453abfdc3481df851e64f105a98d05dda27b5
ljj33 <ljj3347@naver.com>
2024-02-13 17:33:13 +0900

    240213_3

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -9,14 +9,17 @@ export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
   
-
+  console.log('code:',code)
   if (code) {
-    const cookieStore = cookies();
-    const supabase = createClient(cookieStore);
-    await supabase.auth.exchangeCodeForSession(code);
-    return NextResponse.redirect(requestUrl.origin)
+    try{
+      const cookieStore = cookies();
+      const supabase = createClient(cookieStore);
+      await supabase.auth.exchangeCodeForSession(code);
+      return NextResponse.redirect(requestUrl.origin)
+    } catch(error){
+      return NextResponse.redirect(requestUrl.origin)
+    }   
   }
-
   // URL to redirect to after sign in process completes
   return NextResponse.redirect(requestUrl.origin);
 }
